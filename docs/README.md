8aee849a7fffed57a5e9f5794acd355ead87f59c
Luoq1-Xu <gancelot36@gmail.com>
2025-01-22 12:35:52 +0800

    Add echo functionality Rename to Nyx

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Nyx User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/Nyx.java
+++ src/main/java/Nyx.java
@@ -1,6 +1,9 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Nyx {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
         String divider = "____________________________________________________________";
 
         String logo = """
@@ -16,9 +19,16 @@ public class Duke {
         System.out.println("\n" + logo + "\n");
         System.out.println(divider);
         System.out.println("Hello. I am Nyx.\n");
-        System.out.println("What can I do for you?\n");
-        System.out.println(divider);
-        System.out.println("Goodbye. Exiting Nyx.\n" + divider);
+        System.out.println("What can I do for you?\n" + divider);
+        while (true) {
+            String input = scan.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Goodbye. Exiting Nyx.\n" + divider);
+                break;
+            }
+            System.out.println(input);
+        }
+
 
     }
 }
