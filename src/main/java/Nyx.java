80375f1213a512d9fe717a5b60dcac84360152d6
Luoq1-Xu <gancelot36@gmail.com>
2025-01-22 21:04:02 +0800

    Add ability to mark tasks as complete and incomplete

--- src/main/java/Nyx.java
+++ src/main/java/Nyx.java
@@ -5,8 +5,8 @@ public class Nyx {
     public static void main(String[] args) {
 
 
-        HashMap<String, Task> tasks = new HashMap<>();
-
+        HashMap<Integer, Task> tasks = new HashMap<>();
+        HashMap<String, Integer> taskNameToId = new HashMap<>();
         Scanner scan = new Scanner(System.in);
         String divider = "____________________________________________________________";
         String logo = """
@@ -23,24 +23,51 @@ public class Nyx {
         System.out.println(divider);
         System.out.println("Hello. I am Nyx.\n");
         System.out.println("What can I do for you?\n" + divider);
+        int counter = 1;
         while (true) {
             String input = scan.nextLine();
+            String[] splitInput = input.split(" ");
             if (input.equals("bye")) {
                 System.out.println("Goodbye. Exiting Nyx.\n" + divider);
                 break;
             }
+            if (splitInput[0].equals("mark")) {
+                if (splitInput.length == 2) {
+                    int taskId = Integer.parseInt(splitInput[1]);
+                    if (tasks.containsKey(taskId)) {
+                        tasks.get(taskId).markAsComplete();
+                        System.out.println("Task marked as complete: " + tasks.get(taskId) + "\n" + divider);
+                    }
+                } else {
+                    System.out.println("Wrong usage. Correct usage: mark [task id]\n" + divider);
+                }
+                continue;
+            }
+            if (splitInput[0].equals("unmark")) {
+                if (splitInput.length == 2) {
+                    int taskId = Integer.parseInt(splitInput[1]);
+                    if (tasks.containsKey(taskId)) {
+                        tasks.get(taskId).markAsIncomplete();
+                        System.out.println("Task marked as incomplete: " + tasks.get(taskId) + "\n" + divider);
+                    }
+                } else {
+                    System.out.println("Wrong usage. Correct usage: unmark [task id]\n" + divider);
+                }
+                continue;
+            }
             if (input.equals("list")) {
-                int counter = 1;
+                System.out.println("Here is the current list of tasks:");
                 for (Task task : tasks.values()) {
-                    System.out.println(counter + ". " + task);
-                    counter++;
+                    System.out.println(task);
                 }
                 System.out.println(divider);
             } else {
-                if (!tasks.containsKey(input)) {
-                    Task newTask = new Task(input);
-                    tasks.put(input, newTask);
-                    System.out.println("Task created: " + newTask + "\n" + divider);
+                if (!taskNameToId.containsKey(input)) {
+                    Task newTask = new Task(input, counter);
+                    tasks.put(counter, newTask);
+                    taskNameToId.put(input, counter);
+                    counter++;
+                    System.out.println("Task created: " + input + "\n" + divider);
                 } else {
                     System.out.println("Task already exists: " + input + "\n" + divider);
                 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,33 @@
 public class Task {
     private final String name;
+    private final int id;
+    private boolean completed = false;
 
-    public Task(String name) {
+    public Task(String name, int id) {
         this.name = name;
+        this.id = id;
+    }
+
+    public void markAsComplete() {
+        this.completed = true;
+    }
+
+    public void markAsIncomplete() {
+        this.completed = false;
     }
 
     @Override
     public String toString() {
-        return name;
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.id);
+        sb.append(". ");
+        if (this.completed) {
+            sb.append("[X]");
+        } else {
+            sb.append("[ ]");
+        }
+        sb.append(" ");
+        sb.append(this.name);
+        return sb.toString();
     }
 }
