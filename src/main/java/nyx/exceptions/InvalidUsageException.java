c282498bb761e43353e1a20c35cfaec27328725b
Luoq1-Xu <gancelot36@gmail.com>
2025-01-29 23:19:53 +0800

    Add javaDocs for Exceptions package

--- src/main/java/nyx/exceptions/InvalidUsageException.java
+++ src/main/java/nyx/exceptions/InvalidUsageException.java
@@ -1,6 +1,15 @@
 package nyx.exceptions;
 
+/**
+ * Represents an exception that is thrown when a command is used incorrectly.
+ */
 public class InvalidUsageException extends NyxException {
+
+    /**
+     * Constructs a new InvalidUsageException with the specified detail message.
+     *
+     * @param message The detail message.
+     */
     public InvalidUsageException(String message) {
         super(message);
     }
--- src/main/java/nyx/exceptions/UnknownCommandException.java
+++ src/main/java/nyx/exceptions/UnknownCommandException.java
@@ -1,6 +1,15 @@
 package nyx.exceptions;
 
+/**
+ * Represents an exception that is thrown when an unknown command is encountered.
+ */
 public class UnknownCommandException extends NyxException {
+
+    /**
+     * Constructs a new UnknownCommandException with the specified detail message.
+     *
+     * @param message The detail message.
+     */
     public UnknownCommandException(String message) {
         super(message);
     }
